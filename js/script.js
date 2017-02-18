a04d67485b2b2eeb9bc5d8db8a9147c8fc404a08
Elena Kunina <olenakunina@gmail.com>
2017-02-18 23:50:36 +0300

    js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,54 @@
- var link = document.querySelector(".mail-us .btn");
+'use strict';
 
-      var popup = document.querySelector(".modal-content");
-      var close = popup.querySelector(".modal-content-close");
+(function() {
+  var link = document.querySelector(".mail-us .btn");
 
-      link.addEventListener("click", function(event) {
+  var popup = document.querySelector(".modal-content");
+  var close = popup.querySelector(".modal-content-close");
+
+  var form = popup.querySelector("form")
+  var name = popup.querySelector("[name=name]");
+  var email = popup.querySelector("[name=e-mail]");
+
+  var storage = localStorage.getItem("name");
+
+  link.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.add("modal-content-show");
+    
+    if (storage) {
+      name.value = storage
+      email.focus();
+    } else {
+      name.focus();	
+    }
+    
+  });
+
+  close.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.classList.remove("modal-content-show");
+    popup.classList.remove("modal-error");
+  });
+
+  form.addEventListener("submit", function(event) {
+  	 if (!name.value || !email.value) {
         event.preventDefault();
-        popup.classList.add("modal-content-show");
-      });
+        console.log("Нужно ввести имя и e-mail");
+        popup.classList.add("modal-error");
+      } else {
+          localStorage.setItem("name", name.value);
+        }
+    });
 
-      close.addEventListener("click", function(event) {
-        event.preventDefault();
-        popup.classList.remove("modal-content-show");
-      });
+  window.addEventListener("keydown", function(event)
+  	  {
+  	 if (event.keyCode === 27) {
+  	   if (popup.classList.contains("modal-content-show")) {
+  	     popup.classList.remove("modal-content-show");
+  	     popup.classList.remove("modal-error");
+
+  	     }
+  	   }
+  	   });   	
+  })();
